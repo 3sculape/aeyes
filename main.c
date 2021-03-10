8dd1a1fc1ac8f15ed7b8660e25789db9e5508c99
Samuel Quesne <samuel.quesne@epita.fr>
2021-03-10 10:03:03 +0100

    cleaned up code a bit

--- main.c
+++ main.c
@@ -11,31 +11,31 @@
 
 int main(int argc, char *argv[])
 {
-	if(argc != 3)
-		errx(3, "Usage : ./gtk <filename>");
+    if(argc != 3)
+        errx(3, "Usage : ./gtk <filename>");
 
-	SDL_Window		*window;
-	SDL_Renderer	*renderer;
-	SDL_Texture		*texture;
-	SDL_Surface		*surface;
+    SDL_Window		*window;
+    SDL_Renderer	*renderer;
+    SDL_Texture		*texture;
+    SDL_Surface		*surface;
 
-	init(&window, &renderer, 1366, 768);
+    init(&window, &renderer, 1366, 768);
 
-	surface = load(argv[1]);
-	if(surface == NULL)
-	{
-		quit(window, renderer, NULL);
-		errx(3, "Coudn't load %s", argv[1]);
-	}
+    surface = load(argv[1]);
+    if(surface == NULL)
+    {
+        quit(window, renderer, NULL);
+        errx(3, "Coudn't load %s", argv[1]);
+    }
 
-	texture = surface_to_texture(surface, renderer);
+    texture = surface_to_texture(surface, renderer);
 
-	print_texture_to_window(texture, renderer);
+    print_texture_to_window(texture, renderer);
     saveJPG(argv[2], surface);
-	SDL_Delay(3000);
-    
-	SDL_FreeSurface(surface);
-	quit(window, renderer, texture);
+    SDL_Delay(3000);
 
-	return 0;
+    SDL_FreeSurface(surface);
+    quit(window, renderer, texture);
+
+    return 0;
 }
