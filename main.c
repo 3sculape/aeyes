e4dd4d8b19cb31f8dd2a57df0aacf445f33f1ebb
Samuel Quesne <samuel.quesne@epita.fr>
2021-03-14 18:22:59 +0100

    gtk with sdl embed

--- main.c
+++ main.c
@@ -2,40 +2,103 @@
 #include <SDL2/SDL_image.h>
 #include <err.h>
 #include <gtk/gtk.h>
+#include <gdk/gdkx.h>
 #include <stdio.h>
 #include "utils/lib_sdl.h"
 #include "utils/stack.h"
 #include "algos/basic.h"
 #include "algos/convert.h"
 #include "algos/color.h"
+#include "algos/blurs.h"
+
+SDL_Window *sdl_window;
+SDL_Renderer *sdl_renderer;
+SDL_Texture *texture;
+GtkWindow *gtk_window;
+GtkBuilder *builder;
+GError* error;
+GtkDrawingArea *gtk_da;
+void *gdk_window;
+void *window_id;
+
+int resizingEventWatcher(void *data, SDL_Event* event) 
+{
+    if (event->type == SDL_WINDOWEVENT) 
+    {
+        SDL_RenderClear(sdl_renderer);
+        SDL_RenderCopy(sdl_renderer, texture, NULL, NULL);
+        SDL_RenderPresent(sdl_renderer);
+    }
+    return 0;
+}
+
+gboolean on_configure()
+{
+    SDL_Event user_event;
+
+    user_event.type = SDL_WINDOWEVENT;
+    user_event.user.code = 2;
+    SDL_PushEvent(&user_event);
+
+    return True;
+}
 
 int main(int argc, char *argv[])
 {
-    if(argc != 3)
+    if(argc != 2)
         errx(3, "Usage : ./gtk <filename>");
 
-    SDL_Window		*window;
-    SDL_Renderer	*renderer;
-    SDL_Texture		*texture;
-    SDL_Surface		*surface;
+    gtk_init(NULL, NULL);
 
-    init(&window, &renderer, 1366, 768);
 
+    GtkBuilder* builder = gtk_builder_new();
+    error = NULL;
+    if (gtk_builder_add_from_file(builder, "test_ui.glade", &error) == 0)
+    {
+        g_printerr("Error loading file: %s\n", error->message);
+        g_clear_error(&error);
+        return 1;
+    }
+
+    gtk_window = GTK_WINDOW(gtk_builder_get_object(builder, "main_window"));
+
+    gtk_da = GTK_DRAWING_AREA(gtk_builder_get_object(builder, "image_window"));
+
+    gtk_widget_show_all(GTK_WIDGET(gtk_window));
+
+    gdk_window = gtk_widget_get_window(GTK_WIDGET(gtk_da));
+    window_id = (void*)(intptr_t)GDK_WINDOW_XID(gdk_window);
+
+    SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_JPG|IMG_INIT_PNG);
+
+    SDL_Surface *surface;
     surface = load(argv[1]);
     if(surface == NULL)
     {
-        quit(window, renderer, NULL);
         errx(3, "Coudn't load %s", argv[1]);
     }
 
-    texture = surface_to_texture(surface, renderer);
+    sdl_window = SDL_CreateWindowFrom(window_id);
+    if(!sdl_window)
+        errx(1, "Coudn't create window");
 
-    print_texture_to_window(texture, renderer);
-    saveJPG(argv[2], surface);
-    SDL_Delay(3000);
+    sdl_renderer = SDL_CreateRenderer(sdl_window, -1, 0);
+
+    texture = create_texture(sdl_renderer, surface->w, surface->h);
+    texture = surface_to_texture(surface, sdl_renderer);
+
+    SDL_RenderClear(sdl_renderer);
+    SDL_RenderCopy(sdl_renderer, texture, NULL, NULL);
+    SDL_RenderPresent(sdl_renderer);
 
     SDL_FreeSurface(surface);
-    quit(window, renderer, texture);
 
+    SDL_AddEventWatch(resizingEventWatcher, NULL);
+
+    g_signal_connect(gtk_window, "configure-event", G_CALLBACK(on_configure), NULL);
+    gtk_main();
+    quit(sdl_window, sdl_renderer, texture);
     return 0;
+
 }
