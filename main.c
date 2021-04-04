d7108c5b4e819c6f263473f12efcfccf0374affe
Nigel Andrews <nigel.andrews@epita.fr>
2021-04-04 19:30:23 +0200

    exif is now stored in exif structure

--- main.c
+++ main.c
@@ -16,34 +16,38 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
         errx(EXIT_FAILURE, "error ");
-    SDL_Init(SDL_INIT_VIDEO);
+    exif e;
+    exif_init(&e);
+    show_info(argv[1], &e);
+    exif_destroy(&e);
+/*  SDL_Init(SDL_INIT_VIDEO);
     SDL_Renderer* renderer;
     SDL_Window* window;
     create_window(500, 500, &window, &renderer);
-    SDL_Surface* original = load(argv[1]);
+    SDL_Surface* original = load(argv[1]);*/
     //savePNG("original.PNG", original);
     // saveJPG("test.JPG", original);
-    if (original == NULL)
+/*  if (original == NULL)
         errx(EXIT_FAILURE, "Load image returned NULL");
     SDL_Surface* sharpened01 = sharpen(original, 0.1, 50);
     SDL_Surface* sharpened05 = sharpen(original, 0.5, 50);
-    SDL_Surface* sharpened1 = sharpen(original, 1, 50);
+    SDL_Surface* sharpened1 = sharpen(original, 1, 50);*/
 /*    SDL_Surface* sharpened2 = sharpen(original, 2, 50);
     SDL_Surface* sharpened5 = sharpen(original, 5, 50);*/
  /*   if (sharpened == NULL)
         errx(EXIT_FAILURE, "unsharp_masking returned NULL");*/
-    savePNG("sharpened01.PNG", sharpened01);
+  /*savePNG("sharpened01.PNG", sharpened01);
     savePNG("sharpened05.PNG", sharpened05);
-    savePNG("sharpened1.PNG", sharpened1);
+    savePNG("sharpened1.PNG", sharpened1);*/
   /*  savePNG("sharpened2.PNG", sharpened2);
     savePNG("sharpened5.PNG", sharpened5);*/
-    SDL_FreeSurface(original);
+/*  SDL_FreeSurface(original);
     SDL_FreeSurface(sharpened01);
     SDL_FreeSurface(sharpened05);
-    SDL_FreeSurface(sharpened1);
+    SDL_FreeSurface(sharpened1);*/
 /*    SDL_FreeSurface(sharpened2);
     SDL_FreeSurface(sharpened5);*/
-    quit(window, renderer, NULL);
+//   quit(window, renderer, NULL);
     //show_l_histo(original);
     //SDL_FreeSurface(original);
     return 0;
--- utils/lib_exif.h
+++ utils/lib_exif.h
@@ -6,10 +6,31 @@
 #include <libexif/exif-log.h>
 #include <libexif/exif-mem.h>
 
+typedef struct exif
+{
+    char* artist_name;
+    char* camera_model;
+    char* xp_author;
+    char* exposure_time;
+    char* date_time;
+    char* copyright_tag;
+    char* focal_length;
+    char* aperture_value;
+    char* user_comment;
+    char* image_description;
+    char* maker_note;
+} exif;
+
+void exif_init(exif* e);
+
+void exif_destroy(exif* e);
+
+//void exif_reset(exif* e);
+
 void trim_spaces(char* string);
 
-void print_tag(ExifData* data, ExifIfd ifd, ExifTag tag);
+void get_tag(ExifData* data, exif* e, ExifIfd ifd, ExifTag tag);
 
-void show_info(char* path);
+void show_info(char* path, exif* e);
 
 #endif
