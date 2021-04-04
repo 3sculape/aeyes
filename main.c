f3f2fd95bfbd70eaba1d844b3e8d50eacd2d3f0f
Nigel Andrews <nigel.andrews@epita.fr>
2021-04-04 17:20:19 +0200

    changed unsharp mask for blur

--- main.c
+++ main.c
@@ -22,29 +22,29 @@ int main(int argc, char *argv[])
     create_window(500, 500, &window, &renderer);
     SDL_Surface* original = load(argv[1]);
     //savePNG("original.PNG", original);
- /*   // saveJPG("test.JPG", original);
+    // saveJPG("test.JPG", original);
     if (original == NULL)
         errx(EXIT_FAILURE, "Load image returned NULL");
     SDL_Surface* sharpened01 = sharpen(original, 0.1, 50);
     SDL_Surface* sharpened05 = sharpen(original, 0.5, 50);
     SDL_Surface* sharpened1 = sharpen(original, 1, 50);
-    SDL_Surface* sharpened2 = sharpen(original, 2, 50);
-    SDL_Surface* sharpened5 = sharpen(original, 5, 50);
-    if (sharpened == NULL)
-        errx(EXIT_FAILURE, "unsharp_masking returned NULL");
+/*    SDL_Surface* sharpened2 = sharpen(original, 2, 50);
+    SDL_Surface* sharpened5 = sharpen(original, 5, 50);*/
+ /*   if (sharpened == NULL)
+        errx(EXIT_FAILURE, "unsharp_masking returned NULL");*/
     savePNG("sharpened01.PNG", sharpened01);
     savePNG("sharpened05.PNG", sharpened05);
     savePNG("sharpened1.PNG", sharpened1);
-    savePNG("sharpened2.PNG", sharpened2);
-    savePNG("sharpened5.PNG", sharpened5);
+  /*  savePNG("sharpened2.PNG", sharpened2);
+    savePNG("sharpened5.PNG", sharpened5);*/
     SDL_FreeSurface(original);
     SDL_FreeSurface(sharpened01);
     SDL_FreeSurface(sharpened05);
     SDL_FreeSurface(sharpened1);
-    SDL_FreeSurface(sharpened2);
-    SDL_FreeSurface(sharpened5);
-    quit(window, renderer, NULL);*/
-    show_l_histo(original);
-    SDL_FreeSurface(original);
+/*    SDL_FreeSurface(sharpened2);
+    SDL_FreeSurface(sharpened5);*/
+    quit(window, renderer, NULL);
+    //show_l_histo(original);
+    //SDL_FreeSurface(original);
     return 0;
 }
