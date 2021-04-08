cb1f417ddbfadbd630aa35e738641904347f0e0a
Samuel Quesne <samuel.quesne@epita.fr>
2021-04-08 16:42:03 +0200

    change mean blur button from fast gaussian to mean

--- main.c
+++ main.c
@@ -678,7 +678,7 @@ void on_btn_apply_mean_blur_clicked(GtkButton *button, app_widgets *app_wdgts)
     quantity = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(app_wdgts->w_size_mean_blur_spin_btn));
 
     SDL_Surface *surface = texture_to_surface(app_wdgts->texture, sdl_renderer);
-    fast_gaussian_blur(surface, ((int)quantity), 5);
+    box_blur(surface, ((int)quantity));
     update_image(surface, app_wdgts);
     SDL_FreeSurface(surface);
 
